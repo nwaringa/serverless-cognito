7b1e1854118b3be0178be88ae4fc5ba2d6ff172a
marekq <marek.kuczynski@gmail.com>
2019-06-10 01:26:12 +0200

    improved imports for faster startup, added comments, added all user page, added dynamo table for login data, added event.json for sam local testing, improved build script, various bugfixes

--- build.sh
+++ build.sh
@@ -30,32 +30,41 @@ zip -r $s3file . -x *.git* *.DS_Store* *.zip*
 # copy the zip to your s3 bucket
 aws s3 cp $s3file s3://$s3bucket
 
-# deploy the cloudformation template to your account using the s3 artifact path as parameters
+# run sam validate to check the sam template
+echo -e '\n@@@ running sam validate locally to test function\n'
 sam validate
-echo -e 'creating stack '$stackname' in region '$region
+
+# run a local invoke to status page for testing
+echo -e '\n@@@ running a GET request on local lambda using sam cli\n'
+sam local invoke -e event.json
+
+# deploy the cloudformation template to your account using the s3 artifact path as parameters
+echo -e '\n@@@ creating stack '$stackname' in region '$region
 sam deploy --template-file template.yml --stack-name $stackname --region $region --capabilities CAPABILITY_IAM --parameter-overrides BucketName=$s3bucket CodeKey=$s3file
 
 # after 1-2 minutes, you can retrieve the login url from the cloudformation output
-echo -e 'created stack '$stackname' in region '$region
-echo -e 'login url shown below\n'
-url=$(aws cloudformation --region $region describe-stacks --stack-name $stackname --query 'Stacks[0].Outputs[0]')
-echo -e $url
+echo -e '\n@@@ created stack '$stackname' in region '$region
+echo -e '\n@@@ login url shown below'
+url=$(aws cloudformation --region $region describe-stacks --stack-name $stackname --query 'Stacks[0].Outputs[0]' | awk '{print $2}')
+echo -e $url'\n'
+
+# debug for marek - copy a new sam version
+aws s3 cp $s3file s3://marek-serverless/serverless-cognito.zip
 
 # if 'saw' is installed, tail the cloudwatch logs of lambda
 sawcmd=$(which saw)
   
 if [ ${#sawcmd} -ne '0' ]; then
     # retrieve the lambda name in order to trail the cloudwatch log group
-    lname=$(aws cloudformation describe-stack-resources --stack-name cognito --region us-east-1 | grep Lambda::Function | awk '{print $3}');
+    lname=$(aws cloudformation describe-stack-resources --stack-name $stackname --region $region | grep Lambda::Function | awk '{print $3}');
 
     # do a get on the main page
-    #curl $url
+    echo -e '\n@@@ running a GET request to '$url'\n'
+    curl $url
 
     # start saw trailing
-    echo -e 'starting saw trail for '$lname' , CTRL-C to exit'
+    echo -e '\n\n@@@ starting saw trail for '$lname' , CTRL-C to exit'
     saw watch --region $region /aws/lambda/$lname --expand
     exit;
 fi
 
-# debug for marek - disabled by default
-#aws s3 cp $s3file s3://marek-serverless/serverless-cognito.zip
--- lambda.py
+++ lambda.py
@@ -5,28 +5,37 @@
 # coding: utf-8
 
 # import neccesary libraries
-import boto3, datetime, hashlib, re, os, time
+import boto3
+from os import environ
+from time import time
+from re import search
+from hashlib import md5
+from datetime import datetime, timedelta
 from random import randint
 from boto3.dynamodb.conditions import Key, Attr
 from urllib.parse import unquote
 
 # take lambda environment variables for cognito
-clientid    = os.environ['clientid']
-userpoolid  = os.environ['userpoolid']
+clientid    = environ['clientid']
+userpoolid  = environ['userpoolid']
 
+# set up connections with cognito and dynamodb using boto3
 cognito     = boto3.client('cognito-idp')
-dynamo      = boto3.resource('dynamodb').Table(os.environ['dynamotable'])
+dynamo      = boto3.resource('dynamodb').Table(environ['dynamotable'])
 
 # return html with a 200 code, the client headers are printed by default
 def return_html(body, title, head, cookie):
+
+    # return a cookie header if neccesary
     if len(cookie) == 0:
         h    = {'Content-Type': 'text/html', 'charset': 'utf-8'}
     else:
         h    = {'Content-Type': 'text/html', 'charset': 'utf-8', 'Set-Cookie': cookie+' ; Secure; HttpOnly'}
 
+    # return a http 200 response code
     return {
         'statusCode': 200, 
-        'body': '<html><head><style>body {font-family: Arial, Helvetica, sans-serif;}</style></head><body><center><br><h1>'+title+'</h1><br><a href = "'+url+'/home">home</a> | <a href = "'+url+'/login">login</a> | <a href = "'+url+'/register">register</a> | <a href = "'+url+'/profile">your profile</a> | <a href = "'+url+'/logout">logout</a> | '+auth+'<br>'+str(body)+'</center><br></body></html>', 
+        'body': '<html><head><style>body {font-family: Arial, Helvetica, sans-serif;}</style></head><body><center><br><h1>'+title+'</h1><br><a href = "'+url+'/home">home</a> | <a href = "'+url+'/login">login</a> | <a href = "'+url+'/register">register</a> | <a href = "'+url+'/users">all users</a> | <a href = "'+url+'/profile">your profile</a> | <a href = "'+url+'/logout">logout</a> | '+auth+'<br><br><br>'+str(body)+'</center></body></html>', 
         'headers': h
     } 
     
@@ -35,69 +44,51 @@ def get_creds(para):
     user    = ''
     pasw    = ''
     
+    # split the provided username and password
     for y in para.split('&'):
-        if re.search('username', y):
+        if search('username', y):
             user    = unquote(y[9:])
-        elif re.search('password', y):
+        elif search('password', y):
             pasw    = unquote(y[9:])
          
     return user, pasw
-    
-# return html for the /login post page
-def post_login(head, para):
-    user, pasw  = get_creds(para)
-    
-    # the password cannot be shorter than 6 characters in cognito, usernames can be 1 character
-    if len(user) > 1 and len(pasw) > 5:
-        
-        # return a succesful login message or print the exception to the user (invalid credentials, wrong parsed characters, user does not exist)
-        try:
-            r = cognito.initiate_auth(ClientId = os.environ['clientid'],
-                AuthFlow = 'USER_PASSWORD_AUTH', 
-                AuthParameters = {'USERNAME' : user.strip(), 'PASSWORD' : pasw.strip()}
-            )
-            
-            cookie      = make_cookie(user)
-            print(str(r['ResponseMetadata']))
-            x = return_html('logged in succesfully with '+str(user)+'<br>', 'logged in', '', cookie)
-
-        except Exception as e:
-            x = return_html('error with login '+str(e), '', '', '')
-
-        return x
-    
-    else:
-        return return_html('invalid user or password entered, this is what i received:\nusername: '+user+'\npassword: '+pasw, '', head, '')
 
 # check if the cookie is valid based on the full cookie string, return the user
 def check_cookie(x):
     user    = x.split('&')[0].split('=')[1]
     cookie  = x.split('&')[1].split('=')[1]
 
+    # check if the cookie is present in dynamodb
     print('check_cookie: '+str(user)+' '+str(cookie))
     x       = dynamo.query(KeyConditionExpression = Key('user').eq(user), FilterExpression= Key('cookie').eq(str(cookie)))
 
+    # set default username 'none' if no match found
     if x['Count'] == int(0):
         print('not found '+user+' '+cookie)
         return 'none'
 
+    # else return the user for which the cookie was found
     else:
         print('found '+user+' '+cookie)   
         return user
 
 # generate a random cookie for the user and write it to DynamoDB.
 def make_cookie(user):
-    now     = int(time.time())
+
+    # calculate the unix timestamp
+    now     = int(time())
     ttl     = now + 259200
 
-    c       = datetime.datetime.utcnow() + datetime.timedelta(days = 3)
+    # calculate the expiry string for the cookie
+    c       = datetime.utcnow() + timedelta(days = 3)
     exp     = c.strftime("%a, %d %b %Y %H:%M:%S GMT")
 
+    # generate a random string for the cookie
     rand    = str(randint(1000, 100000) * now)
-    cookie  = hashlib.md5(rand.encode('utf-8')).hexdigest()
+    cookie  = md5(rand.encode('utf-8')).hexdigest()
 
     # store the current cookie value in dynamodb
-    dynamo.put_item(TableName = os.environ['dynamotable'], 
+    dynamo.put_item(TableName = environ['dynamotable'], 
         Item = {
             'user'      : user,
             'cookie'    : cookie,
@@ -105,11 +96,12 @@ def make_cookie(user):
         }
     )
     
+    # return the cookie string with expiry date
     x  = 'user='+str(user)+'&cookie='+cookie+'; expires='+exp
 
     return x
 
-# post page for a /register request
+# POST register
 def post_register(head, para):
     user, pasw  = get_creds(para)
     
@@ -121,7 +113,12 @@ def post_register(head, para):
             print(cognito.sign_up(Username = user, Password = pasw, ClientId = clientid, UserAttributes = [{'Name': 'email', 'Value': 'devnull@example.com'}]))
             print(cognito.admin_confirm_sign_up(Username = user, UserPoolId = userpoolid))
 
+            # create a cookie and store it in dynamodb
             cookie  = make_cookie(user)
+
+            # set the user as logged in
+            global auth
+            auth        = 'logged in as '+str(user)+'<br>'
 
             # return the cookie to the browser
             return return_html('created user '+user, 'created user '+user, '', cookie)
@@ -132,26 +129,57 @@ def post_register(head, para):
     else:
         return return_html('invalid user or password entered, this is what i received:\nusername: '+user+'\npassword: '+pasw, head, '')
 
-# return html for /login and /registration paths
+# POST login
+def post_login(head, para):
+    user, pasw  = get_creds(para)
+    
+    # the password cannot be shorter than 6 characters in cognito, usernames can be 1 character. 'none' is a restricted username and disabled for registration.
+    if len(user) > 1 and len(pasw) > 5 and user != 'none':
+        
+        # return a succesful login message or print the exception to the user (invalid credentials, wrong parsed characters, user does not exist)
+        try:
+            r = cognito.initiate_auth(ClientId = environ['clientid'],
+                AuthFlow        = 'USER_PASSWORD_AUTH', 
+                AuthParameters  = {'USERNAME' : user.strip(), 'PASSWORD' : pasw.strip()}
+            )
+
+            cookie              = make_cookie(user)
+
+            # set the user as logged in
+            global auth
+            auth                = 'logged in as '+str(user)+'<br>' 
+
+            x = return_html('logged in succesfully with '+str(user), 'logged in', '', cookie)
+           
+        except Exception as e:
+            x = return_html('error with login '+str(e), '', '', '')
+
+        return x
+    
+    else:
+        return return_html('invalid user or password entered, this is what i received:\nusername: '+user+'\npassword: '+pasw, '', head, '')
+
+# GET login
+# GET registration 
 def get_cred_page(head, txt, user):
 
     if user == 'none':
-        body    = '''<br><form method="post">
-        username \t <input type="text" name="username" /><br />
-        password \t <input type="password" name="password" /><br />
+        body    = '''<form method="post">
+        username \t <input type="text" name="username" /><br>
+        password \t <input type="password" name="password" /><br>
         <input type="submit" /></form>'''
 
     else:
-        body    = '<br>you are already logged in with user '+user+', do you want to <a href = "'+url+'/logout">logout</a> instead?<br>'
+        body    = 'you are already logged in with user '+user+', do you want to <a href = "'+url+'/logout">logout</a> instead?'
 
     return return_html(body, txt, head, '')
 
-# return html for the /profile page
+# GET profile page
 def get_profile_page(head, txt, user, cookie):  
     if user != 'none':  
-        body    = '<br><br>Welcome to your profile '+str(user)+', it\'s great that you made an account.<br>You will see an updated profile page here soon, stay tuned.<br><br>'
+        body    = 'Welcome to your profile '+str(user)+', it\'s great that you made an account.<br><br>You will see an updated profile page here soon, stay tuned.'
     else:
-        body    = '<br><br>Please create an account or login using the links above.<br><br>'
+        body    = 'Please create an account or login using the links above.'
 
     return return_html(body, txt, head, '')
 
@@ -169,18 +197,39 @@ def get_cookie_status(cookie):
     
     return body, user
 
-# get home page
+# GET home page
 def get_home(head, para):
-    body        = '<br><br>Welcome to the Cognito demo page!<br><br>This demo was written by Marek Kuczynski in Python and using the Serverless Application Model.<br><br>Checkout one of the menu options above to get started.'
+    body        = 'Welcome to the Cognito demo page!<br><br>This demo was written by Marek Kuczynski in Python using Cognito, Lambda, API Gateway and Cognito.<br><br>It is using the Serverless Application Model and the sourcecode can be found <a href = "https://github.com/marekq/serverless-cognito" target="_blank">here</a>.<br><br>Checkout one of the menu options above to get started.'
 
     return return_html(body, 'home', head, '')
 
-# logout
+# GET logout
 def get_logout(cookie, user):
     body        = 'logged out '+user
     cookie      = 'user='+str(user)+'&cookie='+cookie+'; expires=Thu, 01 Jan 1970 00:00:00 GMT'
 
+    # set the user as logged out
+    global auth
+    auth        = 'user not logged in<br>'
+
     return return_html(body, 'logged out '+user, 'logged out '+user, cookie)
+
+# GET users
+def get_users(title, user):
+    if len(user) != 0:
+        b       = []
+        x       = cognito.list_users(UserPoolId = userpoolid)
+        body    = '<table width = "600"><tr><th>username</th><th>status</th><th>user create</th><th>user modify</th></tr><tr><td>'
+
+        for y in x['Users']:
+            b.append('</td><td>'.join((y['Username'], y['UserStatus'], y['UserCreateDate'].strftime("%m/%d/%Y %H:%M:%S"), y['UserLastModifiedDate'].strftime("%m/%d/%Y %H:%M:%S"))))
+
+        body    += '</tr><tr><td>'.join(b)
+        body    += '</tr></table>'
+
+        return return_html(body, title, 'all users', '')
+    else:
+        return return_html('you need to login for this functionality', title, 'all users', '')
 
 # lambda handler
 def handler(event, context):
@@ -227,8 +276,13 @@ def handler(event, context):
     elif meth == 'GET' and path == 'logout':
         x   = get_logout(cookie, user)
 
+    # status page
     elif meth == 'GET' and path == 'status':
         x   = return_html('success', 'success', 'success', '')
+
+    # list all users
+    elif meth == 'GET' and path == 'users':
+        x   = get_users('all users', user)
 
     # handle POST requests by submitting the query strings to the api using 'para'
 
